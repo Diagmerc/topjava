62ba070901d85ca3383fa581aeb451e0c9d9ddb5
Diagmerc <91744556+Diagmerc@users.noreply.github.com>
2022-11-15 17:13:28 +0300

    2. add SQL table meals and initMeals in DB

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -2,6 +2,8 @@ DELETE
 FROM user_roles;
 DELETE
 FROM users;
+DELETE
+FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -12,3 +14,10 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories)
+VALUES  ('2022-01-22 21:00', 'ужин', '200'),
+        ('2022-01-22 15:00', 'обед', '300'),
+        ('2022-01-22 10:00', 'завтрак', '500');
+
+
