31b16e755beb4e66bcbf1fcc4bb471f108f27786
Diagmerc <91744556+Diagmerc@users.noreply.github.com>
2023-03-03 21:44:54 +0300

    HW 1.1 fix test

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,15 +2,17 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
-import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.MealTestData.MEAL_MATCHER_TO;
+import static ru.javawebinar.topjava.MealTestData.meals;
 import static ru.javawebinar.topjava.UserTestData.*;
 
 class RootControllerTest extends AbstractControllerTest {
@@ -40,10 +42,10 @@ class RootControllerTest extends AbstractControllerTest {
                 .andExpect(view().name("meals"))
                 .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
                 .andExpect(model().attribute("meals",
-                        new AssertionMatcher<List<Meal>>() {
+                        new AssertionMatcher<List<MealTo>>() {
                             @Override
-                            public void assertion(List<Meal> actual) throws AssertionError {
-                                MEAL_MATCHER.assertMatch(actual, meal7, meal6, meal5, meal4, meal3, meal2, meal1);
+                            public void assertion(List<MealTo> actual) throws AssertionError {
+                                MEAL_MATCHER_TO.assertMatch(actual, MealsUtil.getTos(meals, 2000));
                             }
                         }
                 ));
