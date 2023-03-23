3f95a3699487b4f66373cfdf9f00505d54bd431f
Diagmerc <91744556+Diagmerc@users.noreply.github.com>
2023-03-23 21:32:02 +0300

    just little fix

--- src/main/webapp/WEB-INF/jsp/fragments/headTag.jsp
+++ src/main/webapp/WEB-INF/jsp/fragments/headTag.jsp
@@ -13,6 +13,8 @@
     <link rel="stylesheet" href="webjars/datatables/1.13.1/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="webjars/noty/3.1.4/lib/noty.css"/>
     <link rel="shortcut icon" href="resources/images/icon-meal.png">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
 
     <%--http://stackoverflow.com/a/24070373/548473--%>
     <script src="webjars/jquery/3.6.1/jquery.min.js" defer></script>
@@ -20,4 +22,12 @@
     <script src="webjars/datatables/1.13.1/js/jquery.dataTables.min.js" defer></script>
     <script src="webjars/datatables/1.13.1/js/dataTables.bootstrap4.min.js" defer></script>
     <script src="webjars/noty/3.1.4/lib/noty.min.js" defer></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+<%--    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>--%>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.11/jquery-ui.min.js"></script>
+    <script>
+        $( function() {
+            $( "#datepicker" ).datepicker();
+        } );
+    </script>
 </head>
--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -2,65 +2,120 @@ const mealAjaxUrl = "ui/meals/";
 
 // https://stackoverflow.com/a/5064235/548473
 const ctx = {
-    ajaxUrl: mealAjaxUrl
+    ajaxUrl: mealAjaxUrl,
+    updateTable: function () {
+        $.ajax({
+            type: "GET",
+            url: mealAjaxUrl + "filter",
+            data: $("#filter").serialize()
+        }).done(updateTableByData);
+    }
 };
+
+function clearFilter() {
+    $("#filter")[0].reset();
+    $.get(mealAjaxUrl, updateTableByData);
+}
+
+// http://api.jquery.com/jQuery.ajax/#using-converters
+$.ajaxSetup({
+    converters: {
+        "text json": function (stringData) {
+            var json = JSON.parse(stringData);
+            if (typeof json === 'object') {
+                $(json).each(function () {
+                    if (this.hasOwnProperty('dateTime')) {
+                        this.dateTime = this.dateTime.substr(0, 16).replace('T', ' ');
+                    }
+                });
+            }
+            return json;
+        }
+    }
+});
 
 $(function () {
     makeEditable({
-            ctx,
-            datatableApi: $("#datatable").DataTable({
-                "paging": false,
-                "info": true,
-                "columns": [
-                    {
-                        "data": "dateTime"
-                    },
-                    {
-                        "data": "description"
-                    },
-                    {
-                        "data": "calories"
-                    },
-                    {
-                        "defaultContent": "Edit",
-                        "orderable": false
-                    },
-                    {
-                        "defaultContent": "Delete",
-                        "orderable": false
-                    }
-                ],
-                "order": [
-                    [
-                        0,
-                        "asc"
-                    ]
-                ]
+        "columns": [
+            {
+                "data": "dateTime"
+            },
+            {
+                "data": "description"
+            },
+            {
+                "data": "calories"
+            },
+            {
+                "render": renderEditBtn,
+                "defaultContent": "",
+                "orderable": false
+            },
+            {
+                "render": renderDeleteBtn,
+                "defaultContent": "",
+                "orderable": false
+            }
+        ],
+        "order": [
+            [
+                0,
+                "desc"
+            ]
+        ],
+        "createdRow": function (row, data, dataIndex) {
+            $(row).attr("data-meal-excess", data.excess);
+        }
+    });
+
+    $.datetimepicker.setLocale(localeCode);
+
+//  http://xdsoft.net/jqplugins/datetimepicker/
+    var startDate = $('#startDate');
+    var endDate = $('#endDate');
+    startDate.datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d',
+        formatDate: 'Y-m-d',
+        onShow: function (ct) {
+            this.setOptions({
+                maxDate: endDate.val() ? endDate.val() : false
             })
         }
-    );
-    makeFilter();
-    makeResetFilter();
+    });
+    endDate.datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d',
+        formatDate: 'Y-m-d',
+        onShow: function (ct) {
+            this.setOptions({
+                minDate: startDate.val() ? startDate.val() : false
+            })
+        }
+    });
+
+    var startTime = $('#startTime');
+    var endTime = $('#endTime');
+    startTime.datetimepicker({
+        datepicker: false,
+        format: 'H:i',
+        onShow: function (ct) {
+            this.setOptions({
+                maxTime: endTime.val() ? endTime.val() : false
+            })
+        }
+    });
+    endTime.datetimepicker({
+        datepicker: false,
+        format: 'H:i',
+        onShow: function (ct) {
+            this.setOptions({
+                minTime: startTime.val() ? startTime.val() : false
+            })
+        }
+    });
+
+    $('#dateTime').datetimepicker({
+        format: 'Y-m-d H:i'
+    });
 });
-
-function makeFilter(){
-    $("#buttonFilter").click(function(){
-        $.ajax({
-            type: "GET",
-            url: ctx.ajaxUrl + "getBetween",
-            data: $('#filterForm').serialize()
-        }).done(function () {
-            updateTable();
-            successNoty("Filtered");
-        });
-    });
-}
-
-function makeResetFilter(){
-    $("#resetFilter").click(function(){
-        $("#filterForm").each(function(){
-            $(this).find(":input").val("");
-        });
-        $("#buttonFilter").click();
-    });
-}
